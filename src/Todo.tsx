820fb32123cf6b36582d6cbdae08f3eb6b074d12
hagoromo2000 <revtm000126@gmail.com>
2023-07-23 14:45:56 +0900

    completedに初期値falseを設定

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -5,7 +5,7 @@ type TodoType = {
 };
 
 export const Todo = (props: TodoType) => {
-  const { title, userId, completed } = props;
+  const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}（ユーザー：${userId}）`}</p>;
 };
