849a366946992e1ad5ef0c32e4e13dac09179f90
hagoromo2000 <revtm000126@gmail.com>
2023-07-23 14:39:47 +0900

    Todoの取得処理に型定義を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import axios from "axios";
+import { useState } from "react";
 import "./App.css";
+import { Todo } from "./Todo";
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 function App() {
+  const [todos, setTodos] = useState<TodoType[]>([]); // Here
+
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => {
+        return <Todo title={todo.title} userid={todo.userId} />; // And here
+      })}
     </div>
   );
 }
